e081477b6ace4a8a48abb109b02f10b6f340fb6f
soni9391 <sonisree02@gmail.com>
2025-04-27 12:11:00 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "testdevprdtest"
-    key    = "k8/terraform.tfstate"
-    region = "us-east-1"
-  }
+ #backend "s3" {
+  #  bucket = "testdevprdtest"
+   # key    = "k8/terraform.tfstate"
+    #region = "us-east-1"
+ # }
 
   required_version = ">= 1.6.3"
 }
