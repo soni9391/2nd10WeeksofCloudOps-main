96d45ef412d26d5ce2de7727d63c0cccf138dba4
soni9391 <sonisree02@gmail.com>
2025-04-29 20:33:20 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -36,7 +36,7 @@ provider "aws" {
   }
 
   resource "aws_iam_role" "worker" {
-    name = "veera-eks-worker"
+    name = "veera-eks-worker-1"
 
     assume_role_policy = jsonencode({
       "Version": "2012-10-17",
