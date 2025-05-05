c6611e70cc09b927119cacb720257d6176e5a16c
soni9391 <sonisree02@gmail.com>
2025-05-05 19:05:54 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-// // const API_BASE_URL = "http://localhost:8800";
+// // const API_BASE_URL = "http://13.203.160.163:84";
 // const API_BASE_URL = "http://veera.narni.co.in";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
